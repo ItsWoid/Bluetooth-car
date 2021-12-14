34d7644d93e15ba17fb9e3708229bf7601e2a361
FireLiteD <62186908+FireLiteD@users.noreply.github.com>
2021-12-14 13:59:39 +0200

    Update Car.ino

--- Car.ino
+++ Car.ino
@@ -1,11 +1,13 @@
+#define MAX_SPEED 255
+
 #define MOTOR1_A 2
-#define MOTOR1_B 3  // ШИМ!
+#define MOTOR1_B 3  // PWM
 #define MOTOR2_A 4
-#define MOTOR2_B 5  // ШИМ!
+#define MOTOR2_B 5  // PWM
 #define MOTOR3_A 7
-#define MOTOR3_B 6  // ШИМ!
+#define MOTOR3_B 6  // PWM
 #define MOTOR4_A 8
-#define MOTOR4_B 9  // ШИМ!
+#define MOTOR4_B 9  // PWM
 
 #include <AccelMotor.h>
 AccelMotor motorBL(DRIVER2WIRE, MOTOR1_A, MOTOR1_B, HIGH);
@@ -15,6 +17,14 @@ AccelMotor motorBR(DRIVER2WIRE, MOTOR4_A, MOTOR4_B, HIGH);
 
 #include <Pixy2.h>
 Pixy2 pixy;
+
+#define PS2_DAT A0
+#define PS2_CMD A1
+#define PS2_SEL A2
+#define PS2_CLK A3
+
+#include <PS2X_lib.h>
+PS2X ps2x;
 
 void setup() {
   TCCR2B = 0b00000100;  // x64
@@ -36,16 +46,62 @@ void setup() {
   
 }
 
+int currentMode = 0;
+
 void loop() {
-  int8_t res;
+  static uint32_t tmr;
+  if (millis() - tmr >= 50) {
+    tmr += 50;
+    bool success = ps2x.read_gamepad(false, 0);
+    ps2x.reconfig_gamepad();
 
-  res = pixy.line.getAllFeatures();
+    if (success) {
+      if (currentMode == 0) {
+        int valLX = map(ps2x.Analog(PSS_LX), 0, 256, -MAX_SPEED, MAX_SPEED);
+        int valLY = map(ps2x.Analog(PSS_LY), 256, 0, -MAX_SPEED, MAX_SPEED);
+        int valRX = map(ps2x.Analog(PSS_RX), 0, 256, -MAX_SPEED, MAX_SPEED);
+        int valRY = map(ps2x.Analog(PSS_RY), 256, 0, -MAX_SPEED, MAX_SPEED);
 
-  if (res&LINE_BARCODE) {
-    if (pixy.line.barcodes->m_code==0) {
-      pixy.setLED(255, 0, 0);
+        int dutyFR = valLY + valLX;
+        int dutyFL = valLY - valLX;
+        int dutyBR = valLY - valLX;
+        int dutyBL = valLY + valLX;
+
+        dutyFR += valRY - valRX;
+        dutyFL += valRY + valRX;
+        dutyBR += valRY - valRX;
+        dutyBL += valRY + valRX;
+
+        motorFR.smoothTick(dutyFR);
+        motorBR.smoothTick(dutyBR);
+        motorFL.smoothTick(dutyFL);
+        motorBL.smoothTick(dutyBL);
+      } else if (currentMode == 1) { 
+        int8_t res;
+
+        res = pixy.line.getAllFeatures();
+
+        motorFR.smoothTick(255);
+        motorBR.smoothTick(255);
+        motorFL.smoothTick(255);
+        motorBL.smoothTick(255);
+
+        if (res&LINE_BARCODE) {
+          if (pixy.line.barcodes->m_code==0) {
+            pixy.setLED(255, 0, 0);
+            stopMotors();
+          }
+        } else {
+          pixy.setLED(255, 255, 255);
+        }
+      }
     }
-  } else {
-    pixy.setLED(255, 255, 255);
   }
+}
+
+void stopMotors() {
+  motorFR.setSpeed(0);
+  motorBR.setSpeed(0);
+  motorFL.setSpeed(0);
+  motorBL.setSpeed(0);
 }
